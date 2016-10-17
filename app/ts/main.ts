5d8ad3ab6eda403f73588f2d9e3b7906d96e3b9b
cycjimmy <cycjimmy@gmail.com>
2016-10-17 10:42:01 +0800

    2 space indent

--- app/ts/main.ts
+++ app/ts/main.ts
@@ -1,37 +1,37 @@
 (function () {
-	var doc = document.documentElement;
+  var doc = document.documentElement;
 
-	doc.classList.remove('no-scripts');
-	doc.classList.add('js');
+  doc.classList.remove('no-scripts');
+  doc.classList.add('js');
 }());
 
 (function () {
-	if (!window.localStorage.getItem('hasVisited')) {
-		document.querySelector('.Tour').classList.add('is-active');
-		window.localStorage.setItem('hasVisited', 'true');
-	}
+  if (!window.localStorage.getItem('hasVisited')) {
+    document.querySelector('.Tour').classList.add('is-active');
+    window.localStorage.setItem('hasVisited', 'true');
+  }
 }());
 
 
 class Demo {
-	element: HTMLElement;
+  element: HTMLElement;
 
-	constructor(element: HTMLElement) {
-		this.element = element;
-	}
+  constructor(element: HTMLElement) {
+    this.element = element;
+  }
 
-	load(){
-		this.element.innerHTML = '加载完成！';
-	}
+  load() {
+    this.element.innerHTML = '加载完成！';
+  }
 }
 
 window.onload = () => {
-	let demo = new Demo(document.getElementById('show_load'));
-	demo.load();
+  let demo = new Demo(document.getElementById('show_load'));
+  demo.load();
 
-	//下面代码遍历所有元素加上随机外框色，方面页面元素查看
-	[].forEach.call(document.querySelectorAll("*"),function(a:any){
-		a.style.outline="1px solid #"+(~~(Math.random()*(1<<24))).toString(16)
-	})
+  //下面代码遍历所有元素加上随机外框色，方面页面元素查看
+  [].forEach.call(document.querySelectorAll("*"), function (a: any) {
+    a.style.outline = "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16)
+  })
 
 };
--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,21 @@
  */
 
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	browserSync = require('browser-sync'),
-	useref = require('gulp-useref'),
-	uglify = require('gulp-uglify'),
-	gulpIf = require('gulp-if'),
-	cssnano = require('gulp-cssnano'),
-	svgstore = require('gulp-svgstore'),
-	imagemin = require('gulp-imagemin'),
-	cache = require('gulp-cache'),
-	del = require('del'),
-	runSequence = require('run-sequence'),
-	ts = require('gulp-typescript'),
-	ghPages = require('gulp-gh-pages'),
-    pug = require('gulp-pug');
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync'),
+  useref = require('gulp-useref'),
+  uglify = require('gulp-uglify'),
+  gulpIf = require('gulp-if'),
+  cssnano = require('gulp-cssnano'),
+  svgstore = require('gulp-svgstore'),
+  imagemin = require('gulp-imagemin'),
+  cache = require('gulp-cache'),
+  del = require('del'),
+  runSequence = require('run-sequence'),
+  ts = require('gulp-typescript'),
+  ghPages = require('gulp-gh-pages'),
+  pug = require('gulp-pug');
 
 /** Gulp task syntax
  * gulp.task('task-name', function () {
@@ -28,106 +28,106 @@ var gulp = require('gulp'),
  */
 
 //CSS编译
-gulp.task('sass', function() {
-	return gulp
-		.src('app/sass/**/*.scss')
-		.pipe(sass())                      //编译sass
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'], //自动加前缀
-			cascade: false
-		}))
-		.pipe(gulp.dest('app/style'))      //输出css
-		.pipe(browserSync.reload({         //刷新web服务器
-			stream: true
-		}))
+gulp.task('sass', function () {
+  return gulp
+    .src('app/sass/**/*.scss')
+    .pipe(sass())                      //编译sass
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'], //自动加前缀
+      cascade: false
+    }))
+    .pipe(gulp.dest('app/style'))      //输出css
+    .pipe(browserSync.reload({         //刷新web服务器
+      stream: true
+    }))
 });
 
 
 //TS编译
 gulp.task('ts', function () {
-	return gulp
-		.src('app/ts/**/*.ts')
-		.pipe(ts({
-			noImplicitAny: true
-			//out: 'output.js'
-		}))
-		.pipe(gulp.dest('app/scripts'))    //输出js
-		.pipe(browserSync.reload({         //刷新web服务器
-			stream: true
-		}))
+  return gulp
+    .src('app/ts/**/*.ts')
+    .pipe(ts({
+      noImplicitAny: true
+      //out: 'output.js'
+    }))
+    .pipe(gulp.dest('app/scripts'))    //输出js
+    .pipe(browserSync.reload({         //刷新web服务器
+      stream: true
+    }))
 });
 
 
 //pug输出
 gulp.task('pug', function buildHTML() {
-	return gulp
-		.src('app/pug/*.pug')
-		.pipe(pug({
-			pretty:true
-		}))
-		.pipe(gulp.dest('app'))            //输出html
-		.pipe(browserSync.reload({         //刷新web服务器
-			stream: true
-		}))
+  return gulp
+    .src('app/pug/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('app'))            //输出html
+    .pipe(browserSync.reload({         //刷新web服务器
+      stream: true
+    }))
 });
 
 //svg图标合并
 gulp.task('svgstore', function () {
-	return gulp
-		.src('app/icons/*.svg')
-		.pipe(imagemin())                        //压缩svg
-		.pipe(svgstore())                        //合并svg
-		.pipe(gulp.dest('app/images/icons'));
+  return gulp
+    .src('app/icons/*.svg')
+    .pipe(imagemin())                        //压缩svg
+    .pipe(svgstore())                        //合并svg
+    .pipe(gulp.dest('app/images/icons'));
 });
 
 //browserSync 自动刷新web服务器
-gulp.task('browserSync', function() {
-	browserSync({
-		server: {
-			baseDir: 'app'             //目录
-		}
-	})
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: 'app'             //目录
+    }
+  })
 });
 
 //压缩合并
-gulp.task('useref', function(){
-	return gulp
-		.src('app/*.html')
-		.pipe(useref())                     //合并
-		.pipe(gulpIf('*.js', uglify()))     //压缩JS
-		.pipe(gulpIf('*.css', cssnano()))   //压缩CSS
-		.pipe(gulp.dest('dist'));
+gulp.task('useref', function () {
+  return gulp
+    .src('app/*.html')
+    .pipe(useref())                     //合并
+    .pipe(gulpIf('*.js', uglify()))     //压缩JS
+    .pipe(gulpIf('*.css', cssnano()))   //压缩CSS
+    .pipe(gulp.dest('dist'));
 });
 
 //压缩图片
-gulp.task('images', function(){
-	return gulp
-		.src(['app/images/**/*.+(png|jpg|gif|svg)', '!app/images/icons/**/*'])
-		.pipe(imagemin())                    //压缩图片
-		.pipe(gulp.dest('dist/images'))
+gulp.task('images', function () {
+  return gulp
+    .src(['app/images/**/*.+(png|jpg|gif|svg)', '!app/images/icons/**/*'])
+    .pipe(imagemin())                    //压缩图片
+    .pipe(gulp.dest('dist/images'))
 });
 
 //复制其他
-gulp.task('copyOther', function() {
-	return gulp
-		.src([
-			'app/images/icons/**/*'   //复制图标
-		], {
-			base: 'app'
-		})
-		.pipe(gulp.dest('dist'))
+gulp.task('copyOther', function () {
+  return gulp
+    .src([
+      'app/images/icons/**/*'   //复制图标
+    ], {
+      base: 'app'
+    })
+    .pipe(gulp.dest('dist'))
 });
 
 //清空目录
-gulp.task('clean', function(callback) {
-	del('dist');
-	return cache.clearAll(callback);
+gulp.task('clean', function (callback) {
+  del('dist');
+  return cache.clearAll(callback);
 });
 
 //清空目录，不包含图片
-gulp.task('clean:dist', function(callback){
-	del(['dist/**/*', '!dist/images', '!dist/images/**/*']);
-	return cache.clearAll(callback);
+gulp.task('clean:dist', function (callback) {
+  del(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+  return cache.clearAll(callback);
 });
 
 
@@ -136,12 +136,11 @@ gulp.task('clean:dist', function(callback){
  */
 
 //监听
-gulp.task('watch', ['browserSync', 'sass', 'ts', 'pug'], function(){
-	gulp.watch('app/sass/**/*.scss', ['sass']);                //监听scss文件变化
-	gulp.watch('app/ts/**/*.ts', ['ts']);                      //监听TS文件变化
-	gulp.watch('app/pug/**/*.pug', ['pug']);              	   //监听pug文件变化
+gulp.task('watch', ['browserSync', 'sass', 'ts', 'pug'], function () {
+  gulp.watch('app/sass/**/*.scss', ['sass']);                //监听scss文件变化
+  gulp.watch('app/ts/**/*.ts', ['ts']);                      //监听TS文件变化
+  gulp.watch('app/pug/**/*.pug', ['pug']);              	   //监听pug文件变化
 });
-
 
 
 /** runSequence syntax
@@ -153,22 +152,22 @@ gulp.task('watch', ['browserSync', 'sass', 'ts', 'pug'], function(){
 
 
 gulp.task('default', function (callback) {
-	runSequence(['sass', 'ts', 'pug','browserSync', 'watch'],
-		callback
-	)
+  runSequence(['sass', 'ts', 'pug', 'browserSync', 'watch'],
+    callback
+  )
 });
 
 
 gulp.task('build', function (callback) {
-	runSequence('clean:dist', 'sass', 'ts', 'pug'
-		['useref', 'images', 'copyIcon'],
-		callback
-	)
+  runSequence('clean:dist', 'sass', 'ts', 'pug'
+      ['useref', 'images', 'copyIcon'],
+    callback
+  )
 });
 
 
 //部署到ghPages
-gulp.task('deploy', function() {
-	return gulp.src('./dist/**/*')
-		.pipe(ghPages());
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
